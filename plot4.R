76293eb9242029cd2b2839512238d9683578a92c
Seweryn Sobieszek <serweryn@gmail.com>
2015-08-10 01:02:15 +0200

    Plot 4 completed

--- plot4.R
+++ plot4.R
@@ -20,21 +20,21 @@ transform(data, Time = strptime(Time, "%H:%M:%S"))
 DateTime = strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
 # generating plot
-#png(file = "plot4.png", width = 480, height = 480)
+png(file = "plot4.png", width = 480, height = 480)
 
 par(mfcol = c(2,2))
 
 with(data, {
-    plot(DateTime, Global_active_power, type = "l", xlab="", ylab = "Global Active Power (kilowatts)")
+    plot(DateTime, Global_active_power, type = "l", xlab="", ylab = "Global Active Power")
 
     plot(DateTime, Sub_metering_1, type = "l", xlab="", ylab = "Energy sub metering")
     lines(DateTime, Sub_metering_2, col = "red")
     lines(DateTime, Sub_metering_3, col = "blue")
-    legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    legend("topright", lty = 1, bty="n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
     plot(DateTime, Voltage, type="l", xlab="datetime")
 
     plot(DateTime, Global_reactive_power, type="l", xlab="datetime")
 })
 
-#dev.off()
+dev.off()
